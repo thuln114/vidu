047c8937b0a8430c2d666b7bd1aa6019beb8f5ae
thuln114 <thunle114@gmail.com>
2020-08-13 15:10:21 +0700

    test

---
+++ src/test/vidu.java
@@ -0,0 +1,19 @@
+package test;
+
+public class vidu {
+	private String thu;
+
+	public vidu(String thu) {
+		super();
+		this.setThu(thu);
+	}
+
+	public String getThu() {
+		return thu;
+	}
+
+	public void setThu(String thu) {
+		this.thu = thu;
+	}
+	
+}
